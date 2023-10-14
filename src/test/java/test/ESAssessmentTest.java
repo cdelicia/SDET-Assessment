78c6f4552a5a022134e5fec2f84433424fb420c3
cdelicia <d-a@live.ru>
2023-10-13 20:56:16 -0700

    change preferences for chromedriver

--- src/test/java/test/ESAssessmentTest.java
+++ src/test/java/test/ESAssessmentTest.java
@@ -2,19 +2,16 @@ package test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.annotations.*;
 import source.Driver;
-
-import static source.Constants.expectedNumberOfElements;
 
 public class ESAssessmentTest {
     private WebDriver driver;
     private static final By addElementButton = By.xpath("//button[contains(text(),'Add Element')]");
     private static final By deleteButton = By.xpath("//button[contains(text(),'Delete')]");
     private static final String url = "https://the-internet.herokuapp.com/add_remove_elements/";
+    private static final int expectedNumberOfElements = 5;
 
     @BeforeMethod
     public void driverInitializer() {
